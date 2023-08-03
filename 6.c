4a2f52cf89bb67c6356a2dbcb981b4d49ad40a02
zhc <zhc@qq.com>
2023-08-03 21:36:22 +0800

    test

---
+++ 6.c
@@ -0,0 +1,30 @@
+/*
+ * @Descripttion: Leetcode_code6
+ * @version: 1.0
+ * @Author: zhc
+ * @Date: 2023-08-02 22:39:03
+ * @LastEditors: zhc
+ * @LastEditTime: 2023-08-02 22:40:29
+ */
+
+/*
+将一个给定字符串 s 根据给定的行数 numRows ，
+以从上往下、从左到右进行 Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，
+排列如下：
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+
+示例 1：
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+*/
+
+char * convert(char * s, int numRows){
+
+}
+int main(){
+    return 0;
+}
