444811811cb4e5032c4bbd4514304e9609597231
ZhengHongChuang <2505460258@qq.com>
2024-01-30 14:46:28 +0800

    合并 K 个升序链表

--- 23.c
+++ 23.c
@@ -4,10 +4,8 @@
  * @Author: zhc
  * @Date: 2023-08-14 01:32:23
  * @LastEditors: zhc
- * @LastEditTime: 2023-08-14 01:33:36
+ * @LastEditTime: 2024-01-24 21:26:28
  */
-
-
 /*
 给你一个链表数组，每个链表都已经按升序排列。
 请你将所有链表合并到一个升序链表中，返回合并后的链表。
@@ -22,11 +20,9 @@
 ]
 将它们合并到一个有序链表中得到。
 1->1->2->3->4->4->5->6
-
 示例 2：
 输入：lists = []
 输出：[]
-
 示例 3：
 输入：lists = [[]]
 输出：[]
@@ -37,15 +33,35 @@ struct ListNode
     int val;
     struct ListNode *next;
 };
-
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
-    if (lists==NULL)
-    {
+    if(listsSize==0){
         return NULL;
     }
-    struct ListNode* res = (struct ListNode*)malloc(sizeof(struct ListNode));
-    
-
-    
+    struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    head->next = NULL;
+    struct ListNode* p = head;
+    int min = 0;
+    int flag = 0;
+    while(1){
+        for(int i = 0;i<listsSize;i++){
+            if(lists[i]!=NULL){
+                flag = 1;
+                min = i;
+                break;
+            }
+        }
+        if(flag == 0){
+            break;
+        }
+        for(int i = min+1;i<listsSize;i++){
+            if(lists[i]!=NULL&&lists[i]->val<lists[min]->val){
+                min = i;
+            }
+        }
+        p->next = lists[min];
+        p = p->next;
+        lists[min] = lists[min]->next;
+    }
+    return head->next;
 
 }
