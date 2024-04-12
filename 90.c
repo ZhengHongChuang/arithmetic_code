25f905b3a7f1d153c49315e02de0e53a8ab0693b
ZhengHongChuang <2505460258@qq.com>
2024-04-12 20:10:42 +0800

    子集 II

--- 90.c
+++ 90.c
@@ -4,7 +4,7 @@
  * @Author: zhc
  * @Date: 2024-04-12 16:58:46
  * @LastEditors: zhc
- * @LastEditTime: 2024-04-12 20:04:09
+ * @LastEditTime: 2024-04-12 20:10:37
  */
 /*
 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的 
@@ -27,46 +27,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int tempSize ;
+int tempSize;
 int *temp;
 
 int **res;
 int resSize;
 
 
-int compare(const void *a , const void *b){
-    return(*(int *)a - *(int *)b);
- }
- void dfs(int* nums, int numsSize, int start ,int cur,int** returnColumnSizes){
-    if(tempSize + numsSize - start < cur) return;
-    if(tempSize == cur){
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+void dfs(int* nums, int numsSize, int start, int cur, int** returnColumnSizes) {
+    if (tempSize + numsSize - start < cur) return;
+    if (tempSize == cur) {
         res[resSize] = malloc(sizeof(int) * cur);
         memcpy(res[resSize], temp, sizeof(int) * cur);
         (*returnColumnSizes)[resSize] = cur;
         resSize++;
         return;
     }
-    if(numsSize == start) return;
+    if (numsSize == start) return;
     temp[tempSize++] = nums[start];
     dfs(nums, numsSize, start + 1, cur, returnColumnSizes);
     tempSize--;
-    while(start + 1 < numsSize && nums[start] == nums[start + 1]) start++;
+    while (start + 1 < numsSize && nums[start] == nums[start + 1]) start++;
     dfs(nums, numsSize, start + 1, cur, returnColumnSizes);
 
 
- }
+}
 int** subsetsWithDup(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    qsort(nums,numsSize,sizeof(int),compare);
-    res = malloc(sizeof(int*)*(1<<numsSize));
+    qsort(nums, numsSize, sizeof(int), compare);
+    res = malloc(sizeof(int*)*(1 << numsSize));
     temp = malloc(sizeof(int) * numsSize);
     resSize = 0;
-    for(int i = 0 ;i <= numsSize;i++){
+    *returnColumnSizes = malloc(sizeof(int) * (1 << numsSize));
+    for (int i = 0; i <= numsSize; i++) {
         tempSize = 0;
-        dfs(nums, numsSize, 0,i, returnColumnSizes);
+        dfs(nums, numsSize, 0, i, returnColumnSizes);
     }
     *returnSize = resSize;
     return res;
 }
+
 int main(){
     int nums[] = {1,2,2,2};
     int numsSize = 4;
