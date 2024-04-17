bbefdba8a4072c49b80a5ae6e0f2a05392648e99
ZhengHongChuang <2505460258@qq.com>
2024-04-17 19:16:43 +0800

    二叉树的锯齿形层序遍历

---
+++ 103.c
@@ -0,0 +1,69 @@
+/*
+ * @Descripttion: Leetcode_code
+ * @version: 1.0
+ * @Author: zhc
+ * @Date: 2024-04-17 19:11:38
+ * @LastEditors: zhc
+ * @LastEditTime: 2024-04-17 19:11:39
+ */
+/*
+给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[20,9],[15,7]]
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+示例 3：
+
+输入：root = []
+输出：[]
+*/
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+int** zigzagLevelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    if(root == NULL){
+        *returnSize = 0;
+        return NULL;
+    }
+    *returnSize = 0;
+    struct TreeNode* queue[2000];
+    int **res = (int**)malloc(sizeof(int*) * 2000);
+    *returnColumnSizes = (int*)malloc(sizeof(int) * 2000);
+
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+    int flag = 0;
+    while(front < rear){
+        int size = rear - front;
+        res[*returnSize] = (int*)malloc(sizeof(int) * size);
+        (*returnColumnSizes)[*returnSize] = size;
+        for(int i = 0; i < size; i++){
+            struct TreeNode* node = queue[front++];
+            if(flag == 0){
+                res[*returnSize][i] = node->val;
+            }else{
+                res[*returnSize][size - i - 1] = node->val;
+            }
+            if(node->left != NULL){
+                queue[rear++] = node->left;
+            }
+            if(node->right != NULL){
+                queue[rear++] = node->right;
+            }
+        }
+        flag = 1 - flag;
+        (*returnSize)++;
+    }
+    return res;
+}
